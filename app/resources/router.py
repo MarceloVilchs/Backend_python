fe544bbe409e0932b142c5fac7862c511b0505aa
MarceloVilchs <m.vilchess@duocuc.cl>
2023-11-22 22:50:45 -0300

    Backend createAccount

--- app/resources/router.py
+++ app/resources/router.py
@@ -6,4 +6,6 @@ from app.resources.createAccount import router as resources_router3
 router = APIRouter()
 router.include_router(resources_router, prefix="/example", tags=["example"])
 router.include_router(resources_router2, prefix="/usuarios", tags=["usuarios"])
+
+#llamada del model al router para integracion de fastapi
 router.include_router(resources_router3, prefix="/createAccount", tags=["createAccount"])
